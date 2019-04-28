2d09022b767a6329e8b27a967af3d324a4dab494
kajiken <knskjwr@gmail.com>
2019-04-28 12:23:50 +0900

    Add homebrew/install.sh

---
+++ homebrew/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Homebrew
+
+# Check for Homebrew
+if test ! $(which brew)
+then
+  echo "  Installing Homebrew."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+exit 0
