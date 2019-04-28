db775572213d1cbfca8fa1dc6338e79199e64203
kajiken <knskjwr@gmail.com>
2019-04-28 12:13:01 +0900

    Add hammerspoon file

---
+++ hammerspoon/config/init.lua
@@ -0,0 +1,60 @@
+local function keyCode(key, modifiers)
+   modifiers = modifiers or {}
+   return function()
+      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), true):post()
+      hs.timer.usleep(1000)
+      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), false):post()
+   end
+end
+
+local function keyCodeSet(keys)
+   return function()
+      for i, keyEvent in ipairs(keys) do
+         keyEvent()
+      end
+   end
+end
+
+local function remapKey(modifiers, key, keyCode)
+   hs.hotkey.bind(modifiers, key, keyCode, nil, keyCode)
+end
+
+local function disableAllHotkeys()
+   for k, v in pairs(hs.hotkey.getHotkeys()) do
+      v['_hk']:disable()
+   end
+end
+
+local function enableAllHotkeys()
+   for k, v in pairs(hs.hotkey.getHotkeys()) do
+      v['_hk']:enable()
+   end
+end
+
+local function handleGlobalAppEvent(name, event, app)
+   if event == hs.application.watcher.activated then
+      if name == "iTerm2" then
+         disableAllHotkeys()
+      else
+         enableAllHotkeys()
+      end
+   end
+end
+
+appsWatcher = hs.application.watcher.new(handleGlobalAppEvent)
+appsWatcher:start()
+
+remapKey({'ctrl'}, 'f', keyCode('right'))
+remapKey({'ctrl'}, 'b', keyCode('left'))
+remapKey({'ctrl'}, 'n', keyCode('down'))
+remapKey({'ctrl'}, 'p', keyCode('up'))
+
+remapKey({'ctrl'}, 'w', keyCodeSet({
+  keyCode('left', {'shift', 'option'}),
+  keyCode('delete')
+}))
+remapKey({'ctrl'}, 'd', keyCodeSet({
+  keyCode('left', {'shift', 'command'}),
+  keyCode('delete')
+}))
+remapKey({'ctrl'}, 'h', keyCode('delete'))
