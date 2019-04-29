fdbeb6aab4665504919caadb508d0703f9a45dee
kajiken <knskjwr@gmail.com>
2019-04-29 16:39:32 +0900

    Add Docker autocomplete plugin for zsh

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -1,3 +1,6 @@
 zplug "zplug/zplug", hook-build:'zplug --self-manage'
+
 zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
+
+zplug "plugins/docker", from:oh-my-zsh, lazy:true
