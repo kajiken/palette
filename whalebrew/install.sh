930190718863c1bb2c6ae2514d261d1b0a6eceba
kajiken <knskjwr@gmail.com>
2019-04-29 16:18:12 +0900

    update bootstrap

--- whalebrew/install.sh
+++ whalebrew/install.sh
@@ -16,8 +16,16 @@ info () {
   printf "\r  \033[00;34m>>>\033[0m $1\n"
 }
 
+# Check for Whalebrew
+if test ! $(which whalebrew)
+then
+  echo "  Installing Whalebrew."
+  curl -L "https://github.com/whalebrew/whalebrew/releases/download/0.1.0/whalebrew-$(uname -s)-$(uname -m)" -o /usr/local/bin/whalebrew
+  chmod +x /usr/local/bin/whalebrew
+fi
+
 for package in ${packages[@]}
 do
   info "Installing ${package}"
-  whalebrew install ${package}
+  whalebrew install -f ${package}
 done
