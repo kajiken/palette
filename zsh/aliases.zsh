52f9d8e1ffaf036cb28bec535ce4541f98212bc7
kajiken <knskjwr@gmail.com>
2022-03-26 13:51:04 +0900

    update zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,23 +1,10 @@
-case "${OSTYPE}" in
-    darwin*)
-        alias ls="ls -G -w"
-        ;;
-    linux*)
-        alias ls="ls --color"
-        ;;
-esac
-
+alias ls="ls -G -w"
 alias l='ls'
 alias la='ls -d .*'
 alias ll='ls -l'
 
 alias gs='git status --short --branch'
 
-alias b='bundle'
-alias binit='bundle install --path .bundle/gems'
-alias rinit='bundle install --path .bundle/gems && rake db:create'
 alias c='code'
-alias s='bundle exec rails s'
 
 alias docker-gc='docker system prune'
-alias docker-gc-with-volume='docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /etc:/etc -e REMOVE_VOLUMES=1 spotify/docker-gc'
