ae366336a3764dcbd00b5aa324d6fe498c032682
Kensuke KAJIWARA <kajiken@users.noreply.github.com>
2026-01-08 00:19:10 +0900

    chore: Clean up zsh configuration and remove unused scripts
    - Updated `aliases.zsh` by removing unused devtree shortcuts.
    - Modified `zshrc` to set `XDG_CONFIG_HOME` to a standard location.
    - Deleted obsolete `nvm.zsh` and `rbenv.zsh` files to streamline the configuration.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,15 +4,5 @@ alias la='ls -d .*'
 alias ll='ls -l'
 
 alias gs='git status --short --branch'
-
 alias c='code'
-
 alias docker-gc='docker system prune'
-
-# devtree shortcuts
-alias dt='devtree'
-alias dtc='devtree create'
-alias dtl='devtree list'
-alias dtu='devtree up'
-alias dtr='devtree remove'
-alias dtcd='peco-devtree'
